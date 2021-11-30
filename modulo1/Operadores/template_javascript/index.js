ab3465a7f9601b0b74add76bec99f9db0bd7b48a
Adrieli <adritti@hotmai.com>
2021-11-30 14:00:01 -0300

    exercicio de interpretação de codigo

---
+++ modulo1/Operadores/template_javascript/index.js
@@ -0,0 +1,33 @@
+/*const bool1 = true
+const bool2 = false
+const bool3 = !bool2
+
+let resultado = bool1 && bool2
+console.log("a. ", resultado)
+
+resultado = bool1 && bool2 && bool3 
+console.log("b. ", resultado) 
+
+resultado = !resultado && (bool1 || bool2) 
+console.log("c. ", resultado)
+
+console.log("d. ", typeof resultado)*/
+
+/* 1 - console.log = ("a. " resultado) - a. false 
+console.log = ("b. " resultado) - b. false
+console.log = ("c. " resultado) - c. true 
+console.log = ("d. " resultado) - d. booleano*/ 
+
+/*let primeiroNumero = prompt("Digite um numero!")
+let segundoNumero = prompt("Digite outro numero!")
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma)*/
+
+/* 2 - enquanto não colocar o numero no prompt de primeiroNumero 
+e segundoNumero, nada será impresso no console.log */
+
+/* 3 - ou substitui na variavel primeiroNumero e segundoNumero 
+por um numero real, ou coloca esse numero lá no prompt na pagina de html
+assim definindo o q será somado */
