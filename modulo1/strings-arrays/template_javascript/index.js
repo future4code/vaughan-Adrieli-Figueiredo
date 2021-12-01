39e8a1b214fe505c50ab6aa1ec0889c368cdc8fd
Adrieli <adritti@hotmai.com>
2021-12-01 17:03:57 -0300

    exercicio de interpretação e escrita de codigo

---
+++ modulo1/strings-arrays/template_javascript/index.js
@@ -0,0 +1,43 @@
+let array
+console.log('a. ', array)
+
+array = null
+console.log('b. ', array)
+
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. ', array.length)
+
+let i = 0
+console.log('d. ', array[i])
+
+array[i+1] = 19
+console.log('e. ', array)
+
+const valor = array[i+6]
+console.log('f. ', valor)
+
+/* A. não vai haver resultado
+pois let array n tem nenhum valor atribuido. 
+b. vai imprimir null 
+c. 11
+d. 3 
+e. o 4 vira 19 
+f. 9 */ 
+
+/* const frase = prompt("Digite uma frase")
+
+console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length) */
+
+/* SUBI NUM ONIBUS EM MIRROCOS // 27 letras */
+
+// EXERCICIOS DE ESCRITA DE CODIGO // 
+
+/* 1 - 
+const emailDoUsuario = prompt ("digite seu email")
+const nomeDoUsuario = prompt ("digite seu nome")
+
+console.log ("O email " + emailDoUsuario +" foi cadastrado com sucesso " + " Seja bem vindo(a)"+ nomeDoUsuario) */
+
+let arrayComidas = ['strogonoff','escondidinho','pizza de escarola','coxinha','alface americana'] 
+
+//continuo na proxima pull // 
