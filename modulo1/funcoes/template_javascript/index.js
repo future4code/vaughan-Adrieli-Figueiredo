76fa4b88877d3b918707363a878a7f69b031ea2c
Adrieli <adritti@hotmai.com>
2021-12-02 16:43:28 -0300

    funções - interpretação de codigo

---
+++ modulo1/funcoes/template_javascript/index.js
@@ -0,0 +1,27 @@
+// exercicios de interpretação de codigo//
+
+/*function minhaFuncao(variavel) {
+	return variavel * 5 }
+    
+    
+minhaFuncao(2)
+minhaFuncao(10)
+
+// não tem retorno em console// (?) */ 
+
+/*let textoDoUsuario = prompt("Insira um texto");
+const outraFuncao = function(texto) {
+	return texto.toLowerCase.includes("cenoura")
+}
+
+const resposta = outraFuncao(textoDoUsuario)
+console.log(resposta)*/
+
+/* a) ele vai receber a frase em prompt, na Outra função, vai detectar se tem cenoura
+e deixar a palavra cenoura em minusculo, e imprime no console se a palavra era minuscula ou não (true or false) 
+
+b) 
+i.   Eu gosto de cenoura (true)
+ii.  CENOURA é bom pra vista (true)
+iii. Cenouras crescem na terra (true) */
+
