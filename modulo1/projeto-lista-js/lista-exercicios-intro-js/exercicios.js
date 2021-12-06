6d85d69a49bd8fe07ae54d480c7dc40c3c0399a7
Adrieli <adritti@hotmai.com>
2021-12-05 23:23:49 -0300

    lista de exercicios

--- modulo1/projeto-lista-js/lista-exercicios-intro-js/exercicios.js
+++ modulo1/projeto-lista-js/lista-exercicios-intro-js/exercicios.js
@@ -2,7 +2,6 @@
 
 // EXERCÍCIO 0A
 function soma(num1, num2) {
-  // implemente sua lógica aqui
   return num1 + num2
 }
 
@@ -17,57 +16,84 @@ function imprimeMensagem() {
 // EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
 
 // EXERCÍCIO 01
-function calculaAreaRetangulo() {
-  // implemente sua lógica aqui
-
-}
-
+function calculaAreaRetangulo(altura, largura) {
+  const alturaRetangulo = prompt("insira a altura aqui")
+  const larguraRetangulo = prompt("insira a largura aqui")
+  const areaRetangulo = alturaRetangulo*larguraRetangulo
+  return console.log (areaRetangulo)
+} 
 // EXERCÍCIO 02
 function imprimeIdade() {
-  // implemente sua lógica aqui
-
-}
-
+ let anoAtual = Number(prompt("insira aqui o ano atual"))
+ let anoDeNascimento = Number(prompt("insira aqui seu ano de nascimento"))
+ let idadeAtual = Number(anoAtual-anoDeNascimento)
+ return console.log (idadeAtual)
+} 
 // EXERCÍCIO 03
-function calculaIMC(peso, altura) {
-  // implemente sua lógica aqui
+function calculaIMC() { 
 
+const peso = Number(prompt("insira seu peso aqui"))
+const altura = Number(prompt("insira sua altura aqui"))
+const imc = peso/(altura*altura)
+
+return imc
 }
 
 // EXERCÍCIO 04
 function imprimeInformacoesUsuario() {
-  // implemente sua lógica aqui
-  // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
+  const nome = prompt ("insira aqui seu nome")
+  const idade = prompt ("insira aqui sua idade")
+  const emailUser = prompt ("insira aqui seu email") 
 
+  return console.log (`Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${emailUser}.`)
+  // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
 }
 
 // EXERCÍCIO 05
 function imprimeTresCoresFavoritas() {
-  // implemente sua lógica aqui
+  const cor1 = prompt("insira sua primeira cor favorita")
+  const cor2 = prompt("insira aqui sua segunda cor favorita")
+  const cor3 = prompt("insira aqui sua terceira cor favorita")
 
+  const favColor = [cor1,cor2,cor3]
+  
+  return console.log (favColor)
 }
 
 // EXERCÍCIO 06
 function retornaStringEmMaiuscula(string) {
-  // implemente sua lógica aqui
-
+  const palavra = prompt ("insira uma palavra minuscula")
+  
+  return palavra.toUpperCase()
 }
 
 // EXERCÍCIO 07
 function calculaIngressosEspetaculo(custo, valorIngresso) {
-  // implemente sua lógica aqui
+  const custoTotal = prompt("qual o custo do evento ?")
+  const valorIngressoUnit = prompt("qual o custo unitario dos ingressos")
 
+  const minLucro = custo/valorIngresso
+
+  return minLucro
 }
 
 // EXERCÍCIO 08
 function checaStringsMesmoTamanho(string1, string2) {
-  // implemente sua lógica aqui
+    const comparação = string1.length === string2.length 
 
+    return comparação
 }
 
 // EXERCÍCIO 09
 function retornaPrimeiroElemento(array) {
-  // implemente sua lógica aqui
+  
+  const array1 = prompt("insira uma fruta")
+  const array2 = prompt("insira uma fruta")
+  const array3 = prompt("insira uma fruta")
+
+  const array = array1,array2,array3
+  const itemArray = item[0]
+  return itemArray
 
 }
 
