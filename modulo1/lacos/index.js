08121ad79c96e5feb71476c06cd7677d6faa5c51
Adrieli <adritti@hotmai.com>
2021-12-08 22:16:00 -0300

    ultima alteração

--- modulo1/lacos/index.js
+++ modulo1/lacos/index.js
@@ -1,5 +1,21 @@
 // interpretação de codigo 
 
-//1 
+//1 no console valor sera = a 5 
 
-//
+/* 2 a) vai ser impresso no console todos os numeros maiores q 18
+
+b) teria q por .length. só não consegui descobrir onde exatamente. 
+
+3) imprime o numero de linhas ou o q foi colocado no prompt como asteriscos
+*/
+
+
+// escrita de codigo 
+
+//1 
+let numeroDeBichinhos = Number(prompt("quantos bichinhos você tem ? "))
+if (numeroDeBichinhos = "0"){
+    console.log("você pode adotar seu primeiro bichinho")
+} else if (numeroDeBichinhos < "0"){
+    
+}
