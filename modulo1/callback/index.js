949eb105ea3914488fb4ae8ec28163246fae369f
Adrieli <adritti@hotmai.com>
2021-12-09 16:53:08 -0300

    callbacks

---
+++ modulo1/callback/index.js
@@ -0,0 +1,5 @@
+// exercicio de interpretação de codigo 
+
+// 1) a) vai printar cada nome e apelido na array */
+// 2) a) vai imprimir o nome das meninas, mas o console.log fica undefined
+// 3) a) vai imprimir um novo array com todo mundo que nao tem o apelido chijo
