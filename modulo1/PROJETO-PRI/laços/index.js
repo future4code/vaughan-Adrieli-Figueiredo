8996954662149c8c4f4bb80ea685b48969806e12
Adrieli <adritti@hotmai.com>
2022-01-23 20:11:38 -0300

    laços

---
+++ modulo1/PROJETO-PRI/laços/index.js
@@ -0,0 +1,43 @@
+// // interpretação de codigo 
+
+// //1 será impresso no console valor +1 até 5 
+
+//  2 a) vai ser impresso no console todos os numeros maiores q 18
+
+// b) teria q pedir pro console.log imprimir o indice tbm, e após o console.log colocar um indice++
+
+// 3) quantidadeTotal definirá quantas vezes * será impresso, desde que quantidadeTotal seja maior que quantidadeAtual. 
+// resposta do console ficará 
+// *
+// **
+// ***
+// **** - pois foi digitado 4 na primeira const.
+
+
+
+// escrita de codigo 
+
+// 1 a)
+// let numeroDeBichinhos = Number(prompt("quantos bichinhos você tem ? "))
+// if (numeroDeBichinhos = 0) {
+//     console.log("você pode adotar seu primeiro bichinho")
+// } else if (numeroDeBichinhos > 0 ) {
+//     let array = []
+//     let i = 0 
+//     while (i < numeroDeBichinhos) {
+//         nomeBichinho = prompt("qual o nome do seu bichinho ?")
+//         array.push(nomeBichinho)
+//         i++
+//     }
+//     console.log(array)
+// } 
+
+// 2)
+
+const arrayOriginal = [12, 24, 36, 48, 60]
+
+valorArray = (arrayOriginal) => {
+    
+
+}
+
