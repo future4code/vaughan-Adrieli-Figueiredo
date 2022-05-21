f5638f1e0ed9701a202e98f0e59f9aeca48ca52d
Adrieli Lavratti <adritti@hotmail.com>
2022-05-20 22:51:35 -0300

    case1-Brainn

---
+++ modulo6/case1/case1-brainn/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MegaSena from './Pages/MegaSena';
+
+function App() {
+  return (
+    <MegaSena />
+
+    /*aqui tem que aparecer todos os componentes q eu criar */
+  );
+}
+
+export default App;
---
+++ modulo6/case1/case1-brainn/src/Pages/MegaSena.js
@@ -0,0 +1,14 @@
+class MegaSena extends React.Component{
+    render () {
+        return (
+            <div>
+      <p> mega sena
+      </p>
+           </div>
+            
+            
+        )
+    }
+}
+
+export default MegaSena
